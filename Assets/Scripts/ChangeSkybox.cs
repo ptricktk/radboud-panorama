f94b433e08cd4013d61bcabb97464b2edf1b3316
Patrick Tak <lodzic@gmail.com>
2017-02-17 08:31:06 +0100

    Radboud initial upload
    versie 1.3

---
+++ Assets/Scripts/ChangeSkybox.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ChangeSkybox : MonoBehaviour {
+
+	public Material[] volgendeBox;
+	public int TimeForScene;
+
+	private int i = 0;
+	private float timeToSwitch = 1;
+
+	public Toggle cbxForAutoAdvance;
+
+	// Use this for initialization
+	void Start () {
+
+		timeToSwitch = TimeForScene;
+		StartSwitcher ();
+	}
+
+	void StartSwitcher() {
+		Invoke ("ChangeTheBox", timeToSwitch);
+	}
+
+	void ChangeTheBox () {
+
+		if (cbxForAutoAdvance.isOn) {
+			AdvanceLocation ();
+		}
+
+	}
+
+	public void NextLocation() {
+
+		AdvanceLocation ();
+
+	}
+
+	public void ChangeVRmode() {
+
+		GvrViewer.Instance.VRModeEnabled = !GvrViewer.Instance.VRModeEnabled;
+
+	}
+
+
+	private void AdvanceLocation() {
+		RenderSettings.skybox = volgendeBox [i];
+		i++;
+
+		if (i < volgendeBox.Length) {
+			StartSwitcher ();
+		} else {
+			i = 0;
+			StartSwitcher ();
+		}
+	}
+}
---
+++ Assets/Scripts/EnableDisableVR.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnableDisableVR : MonoBehaviour {
+	public Camera vrCameraLeft;
+	public Camera vrCameraRight;
+	public Camera noVR;
+	public Canvas vrCanvas;
+
+
+	// Use this for initialization
+	public void EnableVR () {
+		vrCameraLeft.enabled = true;
+		vrCameraRight.enabled = true;
+
+		noVR.enabled = false;
+
+		vrCanvas.enabled = false;
+		//Application.LoadLevel (1);
+	}
+	
+	// Update is called once per frame
+	public void DisableVR () {
+		vrCameraLeft.enabled = false;
+		vrCameraRight.enabled = false;
+
+		noVR.enabled = true;
+
+		vrCanvas.enabled = false;
+		//Application.LoadLevel (1);
+	}
+}
